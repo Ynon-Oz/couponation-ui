845e709db58a45c5d86e6ce84036621ad3b711c7
ynon84 <ynonoz@gmail.com>
2021-07-26 21:56:19 +0300

    creation

---
+++ src/Components/LayoutArea/SideNav/SideNav.tsx
@@ -0,0 +1,11 @@
+import "./SideNav.css";
+
+function SideNav(): JSX.Element {
+    return (
+        <div className="SideNav">
+			
+        </div>
+    );
+}
+
+export default SideNav;
---
+++ src/Components/MainArea/Coupons/Coupons.tsx
@@ -0,0 +1,11 @@
+import "./Coupons.css";
+
+function Coupons(): JSX.Element {
+    return (
+        <div className="Coupons">
+			
+        </div>
+    );
+}
+
+export default Coupons;
